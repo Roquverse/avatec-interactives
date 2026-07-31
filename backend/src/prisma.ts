93f99eacb6563d58723dabffd68a263c6a61b8a9
Oluwadamilola Cole <arakunrincole@Oluwadamilolas-MacBook-Pro.local>
2026-07-31 14:55:37 +0100

    fix: restore pg adapter as it is required by this Prisma version

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -3,8 +3,14 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:51214/template1';
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
